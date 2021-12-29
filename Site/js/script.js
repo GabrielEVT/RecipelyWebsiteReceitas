6b150a3bc0f84bf97dc3c4e628b8e53b6f4d702a
GabrielEVT <gabrielsoaresevt@gmail.com>
2021-12-29 15:42:06 -0300

    Criação da página receita completa com slideshow e responsividade implementada

--- Site/js/script.js
+++ Site/js/script.js
@@ -1,82 +1,92 @@
-class MenuNavigation {
-    constructor(mobileMenu, cabecalhoLista, cabecalhoListaItem) {
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.cabecalhoLista = document.querySelector(cabecalhoLista);
-        this.cabecalhoListaItem = document.querySelectorAll(cabecalhoListaItem);
-        this.activeClass = 'active';
-        this.overflowY = true;
-
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    animateLinks() {
-        this.cabecalhoListaItem.forEach((link, index) => {
-          link.style.animation
-            ? (link.style.animation = "")
-            : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-                index / 7 + 0.3
-              }s`);
-        });
-      }
-
-    handleClick() {
-        this.mobileMenu.classList.toggle(this.activeClass);
-        this.cabecalhoLista.classList.toggle(this.activeClass);
-        this.animateLinks();
-        document.body.style.overflowY = this.overflowY ? "hidden" : "visible";
-        this.overflowY = !this.overflowY;
-    }
-
-    addClickEvent() {
-        this.mobileMenu.addEventListener("click", this.handleClick);
-    }
-
-    init() {
-        if(this.mobileMenu) {
-            this.addClickEvent();
+$(document).ready(function(){
+    class MenuNavigation {
+        constructor(mobileMenu, cabecalhoLista, cabecalhoListaItem) {
+            this.mobileMenu = document.querySelector(mobileMenu);
+            this.cabecalhoLista = document.querySelector(cabecalhoLista);
+            this.cabecalhoListaItem = document.querySelectorAll(cabecalhoListaItem);
+            this.activeClass = 'active';
+            this.overflowY = true;
+    
+            this.handleClick = this.handleClick.bind(this);
         }
-        return this;
+    
+        animateLinks() {
+            this.cabecalhoListaItem.forEach((link, index) => {
+              link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+                    index / 7 + 0.3
+                  }s`);
+            });
+          }
+    
+        handleClick() {
+            this.mobileMenu.classList.toggle(this.activeClass);
+            this.cabecalhoLista.classList.toggle(this.activeClass);
+            this.animateLinks();
+            document.body.style.overflowY = this.overflowY ? "hidden" : "visible";
+            this.overflowY = !this.overflowY;
+        }
+    
+        addClickEvent() {
+            this.mobileMenu.addEventListener("click", this.handleClick);
+        }
+    
+        init() {
+            if(this.mobileMenu) {
+                this.addClickEvent();
+            }
+            return this;
+        }
     }
-}
+    
+    const menuNavigation = new MenuNavigation(
+        '.mobile-menu',
+        '.cabecalho-lista',
+        '.cabecalho-lista__item'
+    );
+    
+    menuNavigation.init();
+    
+    const marcador = document.querySelector('.marcador');
+    const menuItem = document.querySelectorAll('.cabecalho-lista__item a');
+    function itemAtual(e) {
+        marcador.style.left = e.offsetLeft+"px";
+        marcador.style.width = e.offsetWidth+"px";
+    }
+    menuItem.forEach(link => {
+        link.addEventListener('mouseover', (e) => {
+            itemAtual(e.target);
+            
+        });
+    });
+    
+    const cabecalho = document.querySelector('.cabecalho');
+    const cabecalhoContainer = document.querySelector('.cabecalho .container');
+    document.addEventListener("scroll", () => {
+        let distTop = window.pageYOffset;
+        if(distTop >= 1){
+            cabecalhoContainer.classList.add('scroll-menu-nav');
+            cabecalho.classList.add('scroll-menu-nav');
+        } else {
+            cabecalho.classList.remove('scroll-menu-nav');
+            cabecalhoContainer.classList.remove('scroll-menu-nav');
+        }
+    });
+    
+    let iconeFavorito = document.querySelectorAll(".icone-favorito path");
+    
+    for(i = 0; i < iconeFavorito.length; i++) {
+        iconeFavorito[i].addEventListener("click", function(){
+            this.classList.toggle("animation-icone-favorito");
+        });
+    }
 
-const menuNavigation = new MenuNavigation(
-    '.mobile-menu',
-    '.cabecalho-lista',
-    '.cabecalho-lista__item'
-);
-
-menuNavigation.init();
-
-const marcador = document.querySelector('.marcador');
-const menuItem = document.querySelectorAll('.cabecalho-lista__item a');
-function itemAtual(e) {
-    marcador.style.left = e.offsetLeft+"px";
-    marcador.style.width = e.offsetWidth+"px";
-}
-menuItem.forEach(link => {
-    link.addEventListener('mouseover', (e) => {
-        itemAtual(e.target);
-        
+    $('.receita__slide-info__imagem').slick({
+        autoplay: true,
+        arrows: false,
+        dots: true,
+        autoplaySpeed: 2500,
+        fade: true,
     });
 });
-
-const cabecalho = document.querySelector('.cabecalho');
-const cabecalhoContainer = document.querySelector('.cabecalho .container');
-document.addEventListener("scroll", () => {
-    let distTop = window.pageYOffset;
-    if(distTop >= 1){
-        cabecalhoContainer.classList.add('scroll-menu-nav');
-        cabecalho.classList.add('scroll-menu-nav');
-    } else {
-        cabecalho.classList.remove('scroll-menu-nav');
-        cabecalhoContainer.classList.remove('scroll-menu-nav');
-    }
-});
-
-let iconeFavorito = document.querySelectorAll(".icone-favorito path");
-
-for(i = 0; i < iconeFavorito.length; i++) {
-    iconeFavorito[i].addEventListener("click", function(){
-        this.classList.toggle("animation-icone-favorito");
-    });
-}
---
+++ Site/receita_completa.php
@@ -0,0 +1,95 @@
+<?php include 'cabecalho_rodape/cabecalho.php'; ?>
+<main class="conteudo-principal pagina-receita-completa">
+    <div class="container">
+        <h1 class="receita__titulo">Bolo de chocolate</h1>
+        <section class="receita__video">
+            <video src="video/video-teste.mp4" controls poster=""></video>
+        </section>
+        <section class="receita__slide-info">
+            <div class="receita__slide-info__imagem">
+                <div class="receita__slide-info__imagem-container">
+                    <img src="img/receita/comida01.jpg" alt="">
+                </div>
+                <div class="receita__slide-info__imagem-container">
+                    <img src="img/receita/comida02.jpg" alt="">
+                </div>
+                <div class="receita__slide-info__imagem-container">
+                    <img src="img/receita/comida03.jpg" alt="">
+                </div>
+                <div class="receita__slide-info__imagem-container">
+                    <img src="img/receita/comida04.jpg" alt="">
+                </div>
+                <div class="receita__slide-info__imagem-container">
+                    <img src="img/receita/comida05.jpg" alt="">
+                </div>
+            </div>
+            <div class="receita__slide-info__descricao">
+                <div>
+                    <h2 class="">
+                        <svg 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            xmlns:xlink="http://www.w3.org/1999/xlink" 
+                            aria-hidden="true" 
+                            focusable="false"
+                            preserveAspectRatio="xMidYMid meet" 
+                            viewBox="0 0 20 20">
+                            <g fill="none">
+                                <path d="M7 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z" fill="#db3535"/>
+                                <path d="M14.45 3.929a.5.5 0 0 1 .706 0l1.414 1.414a.5.5 0 1 1-.707.707L14.45 4.636a.5.5 0 0 1 0-.707z" fill="#db3535"/>
+                                <path d="M16.5 11a7 7 0 1 1-14 0a7 7 0 0 1 14 0zM9 6.5v5a.5.5 0 0 0 1 0v-5a.5.5 0 0 0-1 0z" fill="#db3535"/>
+                            </g>
+                        </svg>
+                        Tempo de preparo
+                    </h2>
+                    <p>2h</p>
+                </div>
+
+                <div>
+                    <h2 class="">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg" 
+                            xmlns:xlink="http://www.w3.org/1999/xlink" 
+                            aria-hidden="true" 
+                            role="img"     
+                            preserveAspectRatio="xMidYMid meet" 
+                            viewBox="0 0 1024 768">
+                                <path 
+                                    d="M992 640H32q-13 0-22.5-9.5T0 608q0-188 116.5-320.5T416 128q13 0 22.5-9.5T448 96t-9.5-22.5T416 64t-22.5-9.5T384 32t9.5-22.5T416 0h192q13 0 22.5 9.5T640 32t-9.5 22.5T608 64t-22.5 9.5T576 96t9.5 22.5T608 128q183 27 299.5 159.5T1024 608q0 13-9.5 22.5T992 640zM32 704h960q13 0 22.5 9.5t9.5 22.5t-9.5 22.5T992 768H32q-13 0-22.5-9.5T0 736t9.5-22.5T32 704z" 
+                                    fill="#db3535"/>
+                        </svg>
+                        Serve
+                    </h2>
+                    <p>6 pessoas</p>
+                </div>
+
+                <div>
+                    <h2 class="">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg" fill="#DB3535" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M20.205 4.791a5.938 5.938 0 0 0-4.209-1.754A5.906 5.906 0 0 0 12 4.595a5.904 5.904 0 0 0-3.996-1.558a5.942 5.942 0 0 0-4.213 1.758c-2.353 2.363-2.352 6.059.002 8.412L12 21.414l8.207-8.207c2.354-2.353 2.355-6.049-.002-8.416z"/>
+                        </svg>
+                        Favoritos
+                    </h2>
+                    <p>2.443</p>
+                </div>
+            </div>
+        </section>
+        <section class="receita__ingredientes">
+            <h2>Ingredientes</h2>
+            <ul>
+                <li>1 xicara (chá) de chocolate em pó</li>
+                <li>2 xícaras (chá) de Farinha de Trigo Tradicional Renata</li>
+                <li>1 xicara (chá) de óleo</li>
+                <li>1 xicara (chá) de leite</li>
+                <li>2 ovos</li>
+                <li>1 colher (sopa) de fermento em pó</li>
+            </ul>
+        </section>
+        <section class="receita__modo-preparo">
+            <h2>Modo de preparo</h2>
+            <p>
+                Em uma batedeira, bata as claras em neve. Junte as gemas, uma a uma, e acrescente o açúcar. Despeje o Leite NINHO aos poucos, sem parar de bater. Incorpore delicadamente a farinha peneirada com o Chocolate em Pó DOIS FRADES e o fermento. Despeje em uma forma redonda (28 cm de diâmetro) untada com manteiga e polvilhada com farinha de trigo e leve para assar em forno médio-alto (200ºC), preaquecido, por cerca de 40 minutos.
+            </p>
+        </section>
+    </div>
+</main>
+<?php include 'cabecalho_rodape/rodape.php'; ?>
